f7cd0efd31bf8aecd6df1f18bb5a10a0b72d4522
Shane <shane@chewielabs.com>
2022-03-20 19:49:31 -0700

    Removed colors from app

--- tutorial/part1/ch3_files/app.py
+++ tutorial/part1/ch3_files/app.py
@@ -16,15 +16,15 @@ slider = dcc.Slider(0, 10, 1)
 # App Layout 
 app.layout = dbc.Container(
     [
-        dbc.Row(dbc.Col(markdown, style={"background-color": '#636EFA'}, width=8)),
+        dbc.Row(dbc.Col(markdown, width=8)),
         dbc.Row(
             [
-                dbc.Col(dropdown, style={"background-color": '#00CC96'}, width = 3), 
-                dbc.Col(slider, style={"background-color": '#EF553B'}, width = 9),
+                dbc.Col(dropdown, width = 3), 
+                dbc.Col(slider, width = 9),
             ]
         ),
-        dbc.Row(dbc.Col(slider, style={"background-color": '#AB63FA'}, width = 6)),
-        dbc.Row(dbc.Col(button, style={"background-color": '#00CC96'}, width = 11)),
+        dbc.Row(dbc.Col(slider, width = 6)),
+        dbc.Row(dbc.Col(button, width = 11)),
     ]
 )
 
