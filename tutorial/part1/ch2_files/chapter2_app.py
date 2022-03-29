7011a42dffff60ec3fe42cdd02ba96c0b5396923
Charming Data <info@charmingdata.org>
2022-03-29 16:18:07 -0400

    Update chapter2_app.py

--- tutorial/part1/ch2_files/chapter2_app.py
+++ tutorial/part1/ch2_files/chapter2_app.py
@@ -3,7 +3,7 @@ from dash import Dash, dcc
 import dash_bootstrap_components as dbc
 
 # (2) Initialise the App --------------------------------------------
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # (3) App Layout --------------------------------------------
 app.layout = dbc.Container([
