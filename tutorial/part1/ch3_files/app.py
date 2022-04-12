7885125b38a78cdbc63e2caebd3007e6069d1294
Charming Data <info@charmingdata.org>
2022-04-12 14:03:10 -0400

    Update app.py

--- tutorial/part1/ch3_files/app.py
+++ tutorial/part1/ch3_files/app.py
@@ -6,25 +6,30 @@ import dash_bootstrap_components as dbc
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Create app components
-markdown = dcc.Markdown('My First app')
-button = html.Button("Button")
-checklist = dcc.Checklist(['New York City', 'Montréal', 'San Francisco'])
-radio = dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'])
-dropdown = dcc.Dropdown(['NYC', 'MTL', 'SF'])
-slider = dcc.Slider(0, 10, 1)
+markdown = dcc.Markdown(children='My First app')
+button = html.Button(children="Button")
+checklist = dcc.Checklist(options=['New York City', 'Montréal', 'San Francisco'])
+radio = dcc.RadioItems(options=['New York City', 'Montréal', 'San Francisco'])
+dropdown = dcc.Dropdown(options=['NYC', 'MTL', 'SF'])
+slider = dcc.Slider(min=0, max=10, step=1)
 
 # App Layout 
 app.layout = dbc.Container(
     [
-        dbc.Row([dbc.Col(markdown, width=8)]),
+        dbc.Row([dbc.Col([markdown], width=8)]),
         dbc.Row(
             [
-                dbc.Col(dropdown, width = 3), 
-                dbc.Col(slider, width = 9),
+                dbc.Col([dropdown], width = 3), 
+                dbc.Col([slider], width = 9),
             ]
         ),
-        dbc.Row([dbc.Col(slider, width = 6)]),
-        dbc.Row([dbc.Col(button, width = 11)]),
+        dbc.Row(
+            [
+                dbc.Col([checklist], width = 6),
+                dbc.Col([radio], width = 6),
+            ]
+        ),
+        dbc.Row([dbc.Col([button], width = 11)]),
     ]
 )
 
