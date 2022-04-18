5ee08f46a4de46c0ea52d0da10cc45403ae95830
Charming Data <info@charmingdata.org>
2022-04-17 23:06:16 -0400

    Update chapter2_app.py

--- tutorial/part1/ch2_files/chapter2_app.py
+++ tutorial/part1/ch2_files/chapter2_app.py
@@ -1,15 +1,15 @@
-# (1) Import packages --------------------------------------------
+# (1) Import packages
 from dash import Dash, dcc
 import dash_bootstrap_components as dbc
 
-# (2) Initialise the App --------------------------------------------
+# (2) Initialise the App
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# (3) App Layout --------------------------------------------
+# (3) App Layout
 app.layout = dbc.Container([
     dcc.Markdown(children='My First App', style={'textAlign': 'center'})
 ])
 
-# (4) Run the App --------------------------------------------
+# (4) Run the App
 if __name__ == '__main__':
     app.run_server()
