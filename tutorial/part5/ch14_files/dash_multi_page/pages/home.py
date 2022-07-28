a230d85659ec721960c0421c7d7c7255eab5df9b
Adam <adam@charming-data.com>
2022-07-28 00:44:22 -0400

    Create home.py

---
+++ tutorial/part5/ch14_files/dash_multi_page/pages/home.py
@@ -0,0 +1,13 @@
+import dash
+from dash import html, dcc
+
+dash.register_page(__name__, path='/')
+
+layout = html.Div(children=[
+    html.H1(children='This is our Home page'),
+
+    html.Div(children='''
+        This is our Home page content.
+    '''),
+
+])
