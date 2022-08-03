1211e2ab5b1914fd0d1c55ae5837374d64629751
Adam <adam@charming-data.com>
2022-08-03 00:10:25 -0400

    Create app_8_2_3.py

---
+++ tutorial/part5/ch14_files/dash_multi_page_2/pages/app_8_2_3.py
@@ -0,0 +1,40 @@
+import dash
+from dash import dcc, callback, Input, Output
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+
+dash.register_page(__name__)
+
+# Data
+df = px.data.gapminder()
+
+# Page Layout
+layout = dbc.Container([
+    dcc.Markdown("# Interactive Dash App with Line Chart"),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(id='country-dropdown',
+                         options=[x for x in df.country.unique()],
+                         multi=True,
+                         value=['Canada', 'Brazil'])
+        ], width=8)
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id='figure1')
+        ], width = 8)
+    ])
+])
+
+
+# Configure Callback
+@callback(
+    Output('figure1','figure'),
+    Input('country-dropdown', 'value')
+)
+def udpate_graph(countries_selected):
+    df_filtered = df[df.country.isin(countries_selected)]
+    fig = px.line(df_filtered, x='year', y='lifeExp', color='country')
+
+    return fig
