a1b8557550582daf695e329c6054476b4d519e14
Adam <adam@charming-data.com>
2022-08-03 00:20:53 -0400

    Update App_10.py

--- tutorial/part5/ch14_files/dash_multi_page_2/pages/App_10.py
+++ tutorial/part5/ch14_files/dash_multi_page_2/pages/App_10.py
@@ -1,5 +1,6 @@
 # Import packages
-from dash import Dash, dash_table, dcc, html, Input, Output, State
+import dash
+from dash import Dash, dash_table, dcc, html, Input, Output, State, callback
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
